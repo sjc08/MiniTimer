732cc826655434facb535cd8a114afbea39b40e2
AlexSJC <cn_sjc@outlook.com>
2024-02-23 21:13:10 +0800

    添加项目文件。

---
+++ MiniTimer/MiniTimer.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace AS.MiniTimer
+{
+    public class MiniTimer
+    {
+        private bool enabled;
+        private Thread thread;
+        private readonly AutoResetEvent are = new AutoResetEvent(false);
+        private readonly Stopwatch stopwatch = new Stopwatch();
+
+        public event Action<MiniTimer> Elapsed;
+
+        public bool Enabled
+        {
+            get => enabled;
+            set
+            {
+                enabled = value;
+                if (enabled)
+                    StartTimer();
+                else
+                    StopTimer();
+            }
+        }
+
+        public int Interval { get; set; }
+
+        public bool IsRunning => thread != null && thread.IsAlive;
+
+        public void Start() => Enabled = true;
+
+        public void Stop() => Enabled = false;
+
+        public void Wait() => thread?.Join();
+
+        public void StopAndWait()
+        {
+            Stop();
+            Wait();
+        }
+
+        private void StartTimer()
+        {
+            if (!IsRunning)
+            {
+                thread = new Thread(TimerThread);
+                thread.Start();
+            }
+        }
+
+        private void StopTimer()
+        {
+            if (IsRunning)
+            {
+                are.Set();
+            }
+        }
+
+        private void TimerThread()
+        {
+            while (enabled)
+            {
+                stopwatch.Restart();
+                Elapsed?.Invoke(this);
+                are.WaitOne(Math.Max(Interval - (int)stopwatch.ElapsedMilliseconds, 0));
+            }
+        }
+    }
+}
